64c59286831c64a55215e2437ac5b3f491590d62
Mohammad Moradi <mohammad.moradi@snapp.cab>
2022-09-19 14:24:46 +0430

    fix: replace ImageInsert with ImageUpload

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -29,10 +29,10 @@ import HtmlComment from '@ckeditor/ckeditor5-html-support/src/htmlcomment.js';
 import HtmlEmbed from '@ckeditor/ckeditor5-html-embed/src/htmlembed.js';
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
+import ImageInsert from '@ckeditor/ckeditor5-image/src/imageinsert';
 import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize.js';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
 import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
@@ -103,10 +103,10 @@ Editor.builtinPlugins = [
 	HtmlEmbed,
 	Image,
 	ImageCaption,
+	ImageInsert,
 	ImageResize,
 	ImageStyle,
 	ImageToolbar,
-	ImageUpload,
 	Indent,
 	IndentBlock,
 	Italic,
@@ -172,7 +172,7 @@ Editor.defaultConfig = {
 			'alignment',
 			'insertTable',
 			'|',
-			'imageUpload',
+			'imageInsert',
 			'mediaEmbed',
 			'link',
 			'|',
